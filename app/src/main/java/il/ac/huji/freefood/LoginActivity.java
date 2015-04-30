bc69446025ca689a650035525de2b2c6b5c14c4f
Orr P <budabass@gmail.com>
2015-04-30 16:32:04 +0300

    Chanbged Login to SignUp

--- app/src/main/java/il/ac/huji/freefood/LoginActivity.java
+++ app/src/main/java/il/ac/huji/freefood/LoginActivity.java
@@ -1,10 +1,15 @@
 package il.ac.huji.freefood;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.parse.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -13,15 +18,24 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
-        ParseUser.logInInBackground("Jerry", "showmethemoney", new LogInCallback() {
-            public void done(ParseUser user, ParseException e) {
-                if (user != null) {
-                    // Hooray! The user is logged in.
-                } else {
-                    // Signup failed. Look at the ParseException to see what happened.
+        try {
+            InputStreamReader inputStreamReader= new InputStreamReader(this.openFileInput("cred.txt"));
+            BufferedReader credentials = new BufferedReader(inputStreamReader);
+            String name = credentials.readLine();
+            String password = credentials.readLine();
+            final Context context = this;
+            ParseUser.logInInBackground("Jerry", "showmethemoney", new LogInCallback() {
+                public void done(ParseUser user, ParseException e) {
+                    if (user != null) {
+                        startActivity(new Intent(context, ChooseFoodActivity.class));
+                    } else {
+                        startActivity(new Intent(context, SignUpActivity.class));
+                    }
                 }
-            }
-        });
+            });
+        } catch (Exception e) {
+            startActivity(new Intent(this, SignUpActivity.class));
+        }
     }
 
 
