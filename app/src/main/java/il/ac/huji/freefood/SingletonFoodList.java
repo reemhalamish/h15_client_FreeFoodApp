84d951cce025bf5ca5fd7c9d3a42896322b36f89
Reem Halamish <reem.halamish@gmail.com>
2015-04-30 19:33:14 +0300

    with pictures in drawable

--- app/src/main/java/il/ac/huji/freefood/SingletonFoodList.java
+++ app/src/main/java/il/ac/huji/freefood/SingletonFoodList.java
@@ -17,11 +17,13 @@ public class SingletonFoodList {
         return mainList;
     }
 
-    public static List<FoodListItem> updateList ( List<FoodListItem> newList)
-    {
-            mainList = newList;
-            return mainList;
+    public static void addToList(FoodListItem item) {
+
     }
+    public static void deleteFromList(int id) {
 
+    }
+    public static void updateFromList(FoodListItem item) {
 
+    }
 }
