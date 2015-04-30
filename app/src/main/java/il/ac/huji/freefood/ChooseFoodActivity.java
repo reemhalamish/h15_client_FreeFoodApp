d37806511ce58b2a598ee941c17ab19d6bda5d7a
Reem Halamish <reem.halamish@gmail.com>
2015-04-30 14:45:02 +0300

    just before trying the list-view of all the foods in the list :)

--- app/src/main/java/il/ac/huji/freefood/ChooseFoodActivity.java
+++ app/src/main/java/il/ac/huji/freefood/ChooseFoodActivity.java
@@ -2,11 +2,17 @@ package il.ac.huji.freefood;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 /**
  * Created by Elyasaf on 4/30/2015.
  */
 public class ChooseFoodActivity extends Activity {
+    List<FoodListItem> actualList;
+    ChooseFoodActivityAdapter aa;
+
 
 
     @Override
@@ -14,6 +20,15 @@ public class ChooseFoodActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_food_one_row);
 
+        FoodListItem food1 = new FoodListItem(1,20,"kaplan","floor 1", "avocado.jpg","a very ripe avocado","avocado");
+        FoodListItem food2 = new FoodListItem(2,15,"shprinzak","floor 3", "rogalachs.jpg", "leftovers from lecture", "rogalachs");
+        FoodListItem food3 = new FoodListItem(3,40,"shprinzak","floor 1", "cakes.jpg", "leftover cacks", "cacks");
+        actualList.add(food1);
+        actualList.add(food2);
+        actualList.add(food3);
+        ListView lv_todoList = (ListView) findViewById(R.id.lv_FoodShowList);
+        aa = new ChooseFoodActivityAdapter(this, R.layout.choose_food_one_row, R.id.choose_food_picture, actualList);
+        lv_todoList.setAdapter(aa);
 
     }
 }
---
+++ app/src/main/java/il/ac/huji/freefood/ChooseFoodActivityAdapter.java
@@ -0,0 +1,51 @@
+package il.ac.huji.freefood;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Reem on 30/04/2015.
+ */
+public class ChooseFoodActivityAdapter extends ArrayAdapter<FoodListItem> {
+    protected int _resurce;
+    protected List<FoodListItem> _objects;
+    protected int _itemId;
+    protected Context _context;
+
+
+    public ChooseFoodActivityAdapter(
+                        Context context,
+                        int resource,
+                        int itemId,
+                        List<FoodListItem> objects) {
+        super(context, resource, itemId, objects);
+        this._context = context;
+        this._resurce = resource;
+        this._itemId = itemId;
+        this._objects = objects;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View view = inflater.inflate(R.layout.choose_food_one_row, parent, false);
+        FoodListItem curItem = _objects.get(position);
+        TextView txtTitle = (TextView) view.findViewById(R.id.choose_food_title);
+        txtTitle.setText(curItem.getTitle());
+
+        ImageView imgPicture = (ImageView) view.findViewById(R.id.choose_food_picture);
+        Drawable actualPicture = Drawable.createFromPath(curItem.getPicture());
+        imgPicture.setImageDrawable(actualPicture);
+
+        return view;
+    }
+
+    }
