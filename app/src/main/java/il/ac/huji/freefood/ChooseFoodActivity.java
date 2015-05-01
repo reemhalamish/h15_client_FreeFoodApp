d32aee72445d037d22ab8f618fb22e398aefea39
Noam Wies <noamwies@gmail.com>
2015-05-01 06:03:23 +0300

    improve UI

--- app/src/main/java/il/ac/huji/freefood/ChooseFoodActivity.java
+++ app/src/main/java/il/ac/huji/freefood/ChooseFoodActivity.java
@@ -66,7 +66,7 @@ public class ChooseFoodActivity extends Activity {
             AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
             FoodListItem curItem = localCopyOfTheList.get(info.position);
             menu.setHeaderTitle(curItem.getDescription());
-            menu.add(ContextMenu.NONE, 0, 0, "delete item - no more food!");
+            menu.add(ContextMenu.NONE, 0, 0, "delete item");
         }
 
     }
@@ -90,7 +90,7 @@ public class ChooseFoodActivity extends Activity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         final FoodListItem item = localCopyOfTheList.get(index);
         String text = item.getDescription();
-        String msg = "You are about to DELETE ''" + text + "'' FOR GOOD. Are you sure? (CAN'T BE UNDONE)";
+        String msg = "You are about to DELETE ''" + text + "'' FOR GOOD. Are you sure? (CAN'T BE UNDONE, click 'delete' only if you saw with your own eyes that there is no food here)";
         builder.setTitle("Really delete?");
         builder.setMessage(msg);
         // Set up the buttons
